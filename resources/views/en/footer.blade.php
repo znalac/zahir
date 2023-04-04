3433d82e026280e54304634a015c331eb115e3f1
znalac <malacznalac@gmail.com>
2023-04-04 16:01:25 +0200

    first commit

---
+++ resources/views/en/footer.blade.php
@@ -0,0 +1,51 @@
+
+<footer class="ftco-footer ftco-bg-dark ftco-section">
+    <div class="container">
+      <div class="row mb-5">
+        <div class="col-md-3">
+          <div class="ftco-footer-widget mb-4">
+            <h2 class="ftco-heading-2">DentaCare.</h2>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          </div>
+          <ul class="ftco-footer-social list-unstyled float-md-left float-lft ">
+            <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+            <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+            <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+          </ul>
+        </div>
+        <div class="col-md-2">
+          <div class="ftco-footer-widget mb-4 ml-md-5">
+            <h2 class="ftco-heading-2">Quick Links</h2>
+            <ul class="list-unstyled">
+              <li><a href="#" class="py-2 d-block">About</a></li>
+              <li><a href="#" class="py-2 d-block">Features</a></li>
+              <li><a href="#" class="py-2 d-block">Projects</a></li>
+              <li><a href="#" class="py-2 d-block">Blog</a></li>
+              <li><a href="#" class="py-2 d-block">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-1 pr-md-1">
+         
+        </div>
+        <div class="col-md-3">
+          <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Office</h2>
+              <div class="block-23 mb-3">
+                <ul>
+                  <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
+                  <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
+                  <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+                </ul>
+              </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 text-center">
+
+          
+        </div>
+      </div>
+    </div>
+  </footer>
---
+++ resources/views/en/index.blade.php
@@ -0,0 +1,441 @@
+@section('title')
+    Home
+@endsection
+@extends('en.master')
+@section('content')
+<section class="home-slider owl-carousel">
+  <div class="slider-item" style="background-image: url('images/photo/ordinacija_2.jpg');">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row slider-text align-items-center" data-scrollax-parent="true">
+        <div class="col-md-6 col-sm-12 ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+          <h1 class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Modern Dentistry in a Calm and Relaxed Environment</h1>
+          <p class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="slider-item" style="background-image: url('images/photo/ordinacija_4.jpg');">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row slider-text align-items-center" data-scrollax-parent="true">
+        <div class="col-md-6 col-sm-12 ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+          <h1 class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Modern Achieve Your Desired Perfect Smile</h1>
+          <p class="mb-4">A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="ftco-intro">
+  <div class="container">
+    <div class="row no-gutters">
+      <div class="col-md-3 color-1 p-4">
+        <h3 class="mb-4">Emergency Cases</h3>
+        <p>A small river named Duden flows by their place and supplies</p>
+        <span class="phone-number">+ (123) 456 7890</span>
+      </div>
+      <div class="col-md-3 color-2 p-4">
+        <h3 class="mb-4">Opening Hours</h3>
+        <p class="openinghours d-flex">
+          <span>Monday - Friday</span>
+          <span>8:00 - 19:00</span>
+        </p>
+        <p class="openinghours d-flex">
+          <span>Saturday</span>
+          <span>10:00 - 17:00</span>
+        </p>
+        <p class="openinghours d-flex">
+          <span>Sunday</span>
+          <span>10:00 - 16:00</span>
+        </p>
+      </div>
+      
+    </div>
+  </div>
+</section>
+
+<section class="ftco-section ftco-services">
+  <div class="container">
+    <div class="row justify-content-center mb-5 pb-5">
+      <div class="col-md-7 text-center heading-section ftco-animate">
+        <h2 class="mb-2">Our Service Keeps you Smile</h2>
+        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+        <div class="media block-6 services d-block text-center">
+          <div class="icon d-flex justify-content-center align-items-center">
+            <span class="flaticon-tooth-1"></span>
+          </div>
+          <div class="media-body p-2 mt-3">
+            <h3 class="heading">Teeth Whitening</h3>
+            <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+          </div>
+        </div>      
+      </div>
+      <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+        <div class="media block-6 services d-block text-center">
+          <div class="icon d-flex justify-content-center align-items-center">
+            <span class="flaticon-dental-care"></span>
+          </div>
+          <div class="media-body p-2 mt-3">
+            <h3 class="heading">Teeth Cleaning</h3>
+            <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+          </div>
+        </div>    
+      </div>
+      <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+        <div class="media block-6 services d-block text-center">
+          <div class="icon d-flex justify-content-center align-items-center">
+            <span class="flaticon-tooth-with-braces"></span>
+          </div>
+          <div class="media-body p-2 mt-3">
+            <h3 class="heading">Quality Brackets</h3>
+            <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+          </div>
+        </div>      
+      </div>
+      <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+        <div class="media block-6 services d-block text-center">
+          <div class="icon d-flex justify-content-center align-items-center">
+            <span class="flaticon-anesthesia"></span>
+          </div>
+          <div class="media-body p-2 mt-3">
+            <h3 class="heading">Modern Anesthetic</h3>
+            <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+          </div>
+        </div>      
+      </div>
+    </div>
+  </div>
+  <div class="container-wrap mt-5">
+    <div class="row d-flex no-gutters">
+      <div class="col-md-6 img" style="background-image: url(images/photo/ordinacija_7.jpg);">
+      </div>
+      <div class="col-md-6 d-flex">
+        <div class="about-wrap">
+          <div class="heading-section heading-section-white mb-5 ftco-animate">
+            <h2 class="mb-2">Dentacare with a personal touch</h2>
+            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+          </div>
+          <div class="list-services d-flex ftco-animate">
+            <div class="icon d-flex justify-content-center align-items-center">
+              <span class="icon-check2"></span>
+            </div>
+            <div class="text">
+              <h3>Well Experience Dentist</h3>
+              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+            </div>
+          </div>
+          <div class="list-services d-flex ftco-animate">
+            <div class="icon d-flex justify-content-center align-items-center">
+              <span class="icon-check2"></span>
+            </div>
+            <div class="text">
+              <h3>High Technology Facilities</h3>
+              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+            </div>
+          </div>
+          <div class="list-services d-flex ftco-animate">
+            <div class="icon d-flex justify-content-center align-items-center">
+              <span class="icon-check2"></span>
+            </div>
+            <div class="text">
+              <h3>Comfortable Clinics</h3>
+              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="ftco-section">
+  <div class="container">
+    <div class="row justify-content-center mb-5 pb-5">
+      <div class="col-md-7 text-center heading-section ftco-animate">
+        <h2 class="mb-3 " >Meet Our Experience Dentist</h2>
+        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-3 col-md-6 d-flex mb-sm-4 ftco-animate">
+        <div class="staff">
+          <div class="img mb-4" style="background-image: url(images/person_5.jpg);"></div>
+          <div class="info text-center">
+            <h3><a href="teacher-single.html">Tom Smith</a></h3>
+            <span class="position">Dentist</span>
+            <div class="text">
+              <p>Far far away, behind the word mountains, far from the countries Vokalia</p>
+              <ul class="ftco-social">
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-google-plus"></span></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 d-flex mb-sm-4 ftco-animate">
+        <div class="staff">
+          <div class="img mb-4" style="background-image: url(images/person_6.jpg);"></div>
+          <div class="info text-center">
+            <h3><a href="teacher-single.html">Mark Wilson</a></h3>
+            <span class="position">Dentist</span>
+            <div class="text">
+              <p>Far far away, behind the word mountains, far from the countries Vokalia</p>
+              <ul class="ftco-social">
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-google-plus"></span></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 d-flex mb-sm-4 ftco-animate">
+        <div class="staff">
+          <div class="img mb-4" style="background-image: url(images/person_7.jpg);"></div>
+          <div class="info text-center">
+            <h3><a href="teacher-single.html">Patrick Jacobson</a></h3>
+            <span class="position">Dentist</span>
+            <div class="text">
+              <p>Far far away, behind the word mountains, far from the countries Vokalia</p>
+              <ul class="ftco-social">
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-google-plus"></span></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 d-flex mb-sm-4 ftco-animate">
+        <div class="staff">
+          <div class="img mb-4" style="background-image: url(images/person_8.jpg);"></div>
+          <div class="info text-center">
+            <h3><a href="teacher-single.html">Ivan Dorchsner</a></h3>
+            <span class="position">System Analyst</span>
+            <div class="text">
+              <p>Far far away, behind the word mountains, far from the countries Vokalia</p>
+              <ul class="ftco-social">
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-google-plus"></span></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row  mt-5 justify-conten-center">
+    
+    </div>
+  </div>
+</section>
+
+<section class="ftco-section ftco-counter img" id="section-counter" style="background-image: url(images/bg_1.jpg);" data-stellar-background-ratio="0.5">
+  <div class="container">
+    <div class="row d-flex align-items-center">
+      <div class="col-md-3 aside-stretch py-5">
+        <div class=" heading-section heading-section-white ftco-animate pr-md-4">
+          <h2 class="mb-3">Achievements</h2>
+          <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+        </div>
+      </div>
+      <div class="col-md-9 py-5 pl-md-5">
+        <div class="row">
+          <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18">
+              <div class="text">
+                <strong class="number" data-number="14">0</strong>
+                <span>Years of Experience</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18">
+              <div class="text">
+                <strong class="number" data-number="4500">0</strong>
+                <span>Qualified Dentist</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18">
+              <div class="text">
+                <strong class="number" data-number="4200">0</strong>
+                <span>Happy Smiling Customer</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18">
+              <div class="text">
+                <strong class="number" data-number="320">0</strong>
+                <span>Patients Per Year</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="ftco-section">
+  <div class="container">
+    <div class="row justify-content-center mb-5 pb-5">
+      <div class="col-md-7 text-center heading-section ftco-animate">
+        <h2 class="mb-3">Our Best Pricing</h2>
+        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3 ftco-animate">
+        <div class="pricing-entry pb-5 text-center">
+          <div>
+            <h3 class="mb-4">Basic</h3>
+            <p><span class="price">$24.50</span> <span class="per">/ session</span></p>
+          </div>
+          <ul>
+            <li>Diagnostic Services</li>
+            <li>Professional Consultation</li>
+            <li>Tooth Implants</li>
+            <li>Surgical Extractions</li>
+            <li>Teeth Whitening</li>
+          </ul>
+          <p class="button text-center"><a href="#" class="golden-btn">Order now</a></p>
+        </div>
+      </div>
+      <div class="col-md-3 ftco-animate">
+        <div class="pricing-entry pb-5 text-center">
+          <div>
+            <h3 class="mb-4">Standard</h3>
+            <p><span class="price">$34.50</span> <span class="per">/ session</span></p>
+          </div>
+          <ul>
+            <li>Diagnostic Services</li>
+            <li>Professional Consultation</li>
+            <li>Tooth Implants</li>
+            <li>Surgical Extractions</li>
+            <li>Teeth Whitening</li>
+          </ul>
+          <p class="button text-center"><a href="#" class="golden-btn" >Order now</a></p>
+        </div>
+      </div>
+      <div class="col-md-3 ftco-animate">
+        <div class="pricing-entry active pb-5 text-center">
+          <div>
+            <h3 class="mb-4">Premium</h3>
+            <p><span class="price">$54.50</span> <span class="per">/ session</span></p>
+          </div>
+          <ul>
+            <li>Diagnostic Services</li>
+            <li>Professional Consultation</li>
+            <li>Tooth Implants</li>
+            <li>Surgical Extractions</li>
+            <li>Teeth Whitening</li>
+          </ul>
+          <p class="button text-center"><a href="#" class="golden-btn" >Order now</a></p>
+        </div>
+      </div>
+      <div class="col-md-3 ftco-animate">
+        <div class="pricing-entry pb-5 text-center">
+          <div>
+            <h3 class="mb-4">Platinum</h3>
+            <p><span class="price">$89.50</span> <span class="per">/ session</span></p>
+          </div>
+          <ul>
+            <li>Diagnostic Services</li>
+            <li>Professional Consultation</li>
+            <li>Tooth Implants</li>
+            <li>Surgical Extractions</li>
+            <li>Teeth Whitening</li>
+          </ul>
+          <p class="button text-center"><a href="#" class="golden-btn">Order now</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+
+<section class="ftco-gallery">
+  <div class="container-wrap">
+    <div class="row no-gutters">
+      <div class="col-md-3 ftco-animate">
+        <a href="#" class="gallery img d-flex align-items-center" style="background-image: url(images/photo/ordinacija_2.jpg);">
+          
+        </a>
+      </div>
+      <div class="col-md-3 ftco-animate">
+        <a href="#" class="gallery img d-flex align-items-center" style="background-image: url(images/photo/ordinacija_4.jpg);">
+         
+        </a>
+      </div>
+      <div class="col-md-3 ftco-animate">
+        <a href="#" class="gallery img d-flex align-items-center" style="background-image: url(images/photo/ordinacija_3.jpg);">
+      
+        </a>
+      </div>
+      <div class="col-md-3 ftco-animate">
+        <a href="#" class="gallery img d-flex align-items-center" style="background-image: url(images/photo/ordinacija_1.jpg);">
+        
+        </a>
+      </div>
+    </div>
+    <div class="row no-gutters">
+      <div class="col-md-3 ftco-animate">
+        <a href="#" class="gallery img d-flex align-items-center" style="background-image: url(images/photo/ordinacija_5.jpg);">
+          
+        </a>
+      </div>
+      <div class="col-md-3 ftco-animate">
+        <a href="#" class="gallery img d-flex align-items-center" style="background-image: url(images/photo/ordinacija_6.jpg);">
+        
+        </a>
+      </div>
+      <div class="col-md-3 ftco-animate">
+        <a href="#" class="gallery img d-flex align-items-center" style="background-image: url(images/photo/ordinacija_7.jpg);">
+       
+        </a>
+      </div>
+      <div class="col-md-3 ftco-animate">
+        <a href="#" class="gallery img d-flex align-items-center" style="background-image: url(images/photo/ordinacija_8.jpg);">
+         
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="ftco-section">
+  <div class="container">
+    <div class="row justify-content-center mb-5 pb-3">
+      <div class="col-md-7 text-center heading-section ftco-animate">
+    
+      </div>
+    </div>
+   
+  </div>
+</section>
+
+
+
+
+@endsection
